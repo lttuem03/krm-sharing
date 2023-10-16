d3b49394dd59b0694a6fac89202345be6d857d82
lttuem03 <lt.tuem.2003@gmail.com>
2023-10-17 00:11:03 +0700

    created templates folder

--- src/app.py
+++ src/app.py
@@ -1,2 +1,10 @@
+from flask import Flask
 
-print("GitHub introduction")
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<h1>Chào mừng đến với krm-sharing</h1>"
+
+if __name__ == "__main__":
+    app.run()
