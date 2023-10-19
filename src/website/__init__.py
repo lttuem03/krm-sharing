34597ae3e5f36d3713f72affdfc64515d48e8b8d
Yoursha <quangkhai26101984@gmail.com>
2023-10-19 22:59:51 +0700

    some authentication shit ( not finished)

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -12,9 +12,24 @@ def launch_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'afgjsasdlgkaskwkroa'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
