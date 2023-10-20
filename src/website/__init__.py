240e518e791fe8a6ffb76c8bd7a10624099ba9aa
lttuem03 <lt.tuem.2003@gmail.com>
2023-10-20 20:08:38 +0700

    remove flask login for now (thing work)

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -18,7 +18,7 @@ def launch_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
+    f""" rom website.models import User
 
     with app.app_context():
         db.create_all()
@@ -27,9 +27,9 @@ def launch_app():
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
+ """
 
     return app
--- src/website/auth.py
+++ src/website/auth.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
+#from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
-from flask_login import login_user, login_required, logout_user, current_user
+from website import db
+#from flask_login import login_user, login_required, logout_user, current_user
 import re
 
 
@@ -12,39 +12,39 @@ auth = Blueprint('auth', __name__)
 @auth.route('/register/', methods=['GET', 'POST'])
 def register():
 
-    if request.method == 'POST':
-        emailpattern = '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$'
-        email = request.form.get('email')
-        username = request.form.get('username')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Email đã được đăng ký.', category='error')
-        elif not re.match(emailpattern, email):
-            flash('Định dạng email sai.', category='error')
-        elif len(username) < 2:
-            flash('Không cho phép username 1 ký tự.', category='error')
-        elif password1 != password2:
-            flash('Mật khẩu không khớp.', category='error')
-        elif len(password1) < 7:
-            flash('Mật khẩu phải chứa ít nhất 7 ký tự.', category='error')
-        else:
-            new_user = User(email=email, username=username, password=generate_password_hash(
-                password1, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            flash('Tạo tài khoản thành công!', category='success')
-            return redirect(url_for('views.home'))
+    #if request.method == 'POST':
+    #    emailpattern = '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$'
+    #    email = request.form.get('email')
+    #    username = request.form.get('username')
+    #    password1 = request.form.get('password1')
+    #    password2 = request.form.get('password2')
+#
+    #    user = User.query.filter_by(email=email).first()
+    #    if user:
+    #        flash('Email đã được đăng ký.', category='error')
+    #    elif not re.match(emailpattern, email):
+    #        flash('Định dạng email sai.', category='error')
+    #    elif len(username) < 2:
+    #        flash('Không cho phép username 1 ký tự.', category='error')
+    #    elif password1 != password2:
+    #        flash('Mật khẩu không khớp.', category='error')
+    #    elif len(password1) < 7:
+    #        flash('Mật khẩu phải chứa ít nhất 7 ký tự.', category='error')
+    #    else:
+    #        new_user = User(email=email, username=username, password=generate_password_hash(
+    #            password1, method='sha256'))
+    #        db.session.add(new_user)
+    #        db.session.commit()
+    #        login_user(new_user, remember=True)
+    #        flash('Tạo tài khoản thành công!', category='success')
+    #        return redirect(url_for('views.home'))
 
     return render_template('register.html', user=current_user)
 
 
 @auth.route('/login/', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    """ if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
 
@@ -58,5 +58,5 @@ def login():
                 flash('Sai mật khẩu.', category='error')
         else:
             flash('Email không tồn tại.', category='error')
-
+ """
     return render_template("login.html", user=current_user)
