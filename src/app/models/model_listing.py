ba89a97a05562c2487c1cfead13b736ef9ca40a0
Yoursha <quangkhai26101984@gmail.com>
2024-01-17 19:01:29 +0700

    Listing model and query

---
+++ src/app/models/model_listing.py
@@ -0,0 +1,36 @@
+import datetime
+
+from app.models import db
+
+
+class Listing(db.Model):
+    id = db.Column("id", db.Integer, nullable=False, primary_key=True)
+    name = db.Column(db.Unicode(120), nullable=False, unique=False)
+    post_id = db.Column(
+        db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date_posted = db.Column(
+        db.DateTime, nullable=False, unique=False, default=datetime.datetime.now())
+    doc_type = db.Column(db.Unicode(24), nullable=True, unique=False)
+    subject = db.Column(db.Unicode(100), nullable=True, unique=False)
+    school = db.Column(db.Unicode(100), nullable=True, unique=False)
+    author = db.Column(db.Unicode(64), nullable=True, unique=False)
+    year = db.Column(db.Integer, nullable=True, unique=False)
+    description = db.Column(db.Unicode(200), nullable=True,
+                            unique=False, default="<Mô tả trống>")
+    status = db.Column(db.Unicode, nullable=False, unique=False, default="Mới")
+    price = db.Column(db.Integer, nullable=False, unique=False, default=0)
+    location = db.Column(db.Unicode, nullable=False, unique=False, default='')
+    # image folder [krm-id] + (tên tài liệu)
+    foldername = db.Column(db.String(256), nullable=False,
+                           unique=True, default='')
+    # statistics
+    view_count = db.Column(db.Integer, nullable=False, unique=False, default=0)
+    buy_count = db.Column(
+        db.Integer, nullable=False, unique=False, default=0)
+
+    # update rating:
+    #   rating = (rating * rating_count + new rate value) / (rating_count + 1)
+    #   rating_count = rating_count + 1
+    rating = db.Column(db.Float, nullable=False, unique=False, default=0)
+    rating_count = db.Column(
+        db.Integer, nullable=False, unique=False, default=0)
--- src/app/models/query_engine.py
+++ src/app/models/query_engine.py
@@ -2,30 +2,51 @@ from flask_sqlalchemy.query import Query
 
 from app.models.model_user import User
 from app.models.model_document import Document
+from app.models.model_listing import Listing
 from app.models.table_bookmarking import BookmarkingTable
 
-User_column_map = { "id": User.id,
-                    "name": User.name,
-                    "email": User.email,
-                    "username": User.username,
-                    "hased_password": User.hased_password }
+User_column_map = {"id": User.id,
+                   "name": User.name,
+                   "email": User.email,
+                   "username": User.username,
+                   "hased_password": User.hased_password}
 
-Document_column_map = { "id": Document.id,
-                        "name": Document.name,
-                        "uploader_id": Document.uploader_id,
-                        "date_uploaded": Document.date_uploaded,
-                        "type": Document.type,
-                        "subject": Document.subject,
-                        "school": Document.school,
-                        "author": Document.author,
-                        "year": Document.year,
-                        "description": Document.description,
-                        "filename": Document.filename,
-                        "file_size": Document.file_size,
-                        "view_count": Document.view_count,
-                        "download_count": Document.download_count,
-                        "rating": Document.rating,
-                        "rating_count": Document.rating_count}
+Document_column_map = {"id": Document.id,
+                       "name": Document.name,
+                       "uploader_id": Document.uploader_id,
+                       "date_uploaded": Document.date_uploaded,
+                       "type": Document.type,
+                       "subject": Document.subject,
+                       "school": Document.school,
+                       "author": Document.author,
+                       "year": Document.year,
+                       "description": Document.description,
+                       "filename": Document.filename,
+                       "file_size": Document.file_size,
+                       "view_count": Document.view_count,
+                       "download_count": Document.download_count,
+                       "rating": Document.rating,
+                       "rating_count": Document.rating_count}
+
+Listing_column_map = {"id": Listing.id,
+                      "name": Listing.name,
+                      "post_id": Listing.post_id,
+                      "date_posted": Listing.date_posted,
+                      "doc_type": Listing.doc_type,
+                      "subject": Listing.subject,
+                      "school": Listing.school,
+                      "author": Listing.author,
+                      "year": Listing.year,
+                      "description": Listing.description,
+                      "status": Listing.status,
+                      "price": Listing.price,
+                      "location": Listing.location,
+                      "filename": Listing.foldername,
+                      "view_count": Listing.view_count,
+                      "buy_count": Listing.buy_count,
+                      "rating": Listing.rating,
+                      "rating_count": Listing.rating_count}
+
 
 class QueryEngine():
     @staticmethod
@@ -34,7 +55,7 @@ class QueryEngine():
         if column not in User_column_map:
             print("User query: Column not exists!")
             return None
-        
+
         return User.query.filter(User_column_map[column] == value).first()
 
     @staticmethod
@@ -43,7 +64,7 @@ class QueryEngine():
         if column not in User_column_map:
             print("Users query: Column not exists!")
             return None
-        
+
         return User.query.filter(User_column_map[column] == value)
 
     @staticmethod
@@ -52,23 +73,40 @@ class QueryEngine():
         if column not in Document_column_map:
             print("Document query: Column not exists!")
             return None
-        
+
         return Document.query.filter(Document_column_map[column] == value).first()
-    
+
     @staticmethod
     def query_Documents_by(column, value) -> Query:
         """ Return all rows of the Document query result """
         if column not in Document_column_map:
             print("Documents query: Column not exists!")
             return None
-        
+
         return Document.query.filter(Document_column_map[column] == value)
-    
+
+    @staticmethod
+    def query_Listing_by(column, value) -> Listing:
+        """ Return row of the Listing query result """
+        if column not in Listing_column_map:
+            print("Listing query : Column not exists!")
+            return None
+        return Listing.query.filter(Listing_column_map[column] == value).first()
+
+    @staticmethod
+    def query_all_Listing_by(column, value) -> Query:
+        """ Return all rows of the Listing query result """
+        if column not in Listing_column_map:
+            print("All Listing query: Column not exists!")
+            return None
+
+        return Listing.query.filter(Listing_column_map[column] == value)
+
     @staticmethod
     def query_Bookmarking_Table(user_id_value, document_id_value):
         return BookmarkingTable.query.filter(BookmarkingTable.user_id == user_id_value,
                                              BookmarkingTable.document_id == document_id_value).first()
-    
+
     @staticmethod
     def query_Bookmarking_Table_filter_by_user_id(user_id_value):
         return BookmarkingTable.query.filter(BookmarkingTable.user_id == user_id_value)
